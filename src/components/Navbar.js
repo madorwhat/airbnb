d3246702b80503af2a799513aa0fbb3351e310c5
madorwhat <awesomebirdiepod@gmail.com>
2023-02-05 12:54:19 +0530

    adding all files

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../images/airbnb1.png" classname="nav--logo" />
+            
+        </nav>
+    )
+}
